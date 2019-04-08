63d351771e85888b7e47f720216ba39abb99f468
Xun Wang <10509759@qq.com>
2019-04-08 18:39:20 +0800

    Update add new

--- eqcoin/src/main/java/com/eqzip/eqcoin/keystore/AddressTool.java
+++ eqcoin/src/main/java/com/eqzip/eqcoin/keystore/AddressTool.java
@@ -1,6 +1,18 @@
 /**
- * EQCoin core - EQZIP's EQCoin core library
- * @copyright 2018 EQZIP Inc.  All rights reserved...
+ * EQCoin core - EQCOIN Foundation's EQCoin core library
+ * @copyright 2018-present EQCOIN Foundation All rights reserved...
+ * Copyright of all works released by EQCOIN Foundation or jointly released by
+ * EQCOIN Foundation with cooperative partners are owned by EQCOIN Foundation
+ * and entitled to protection available from copyright law by country as well as
+ * international conventions.
+ * Attribution — You must give appropriate credit, provide a link to the license.
+ * Non Commercial — You may not use the material for commercial purposes.
+ * No Derivatives — If you remix, transform, or build upon the material, you may
+ * not distribute the modified material.
+ * For any use of above stated content of copyright beyond the scope of fair use
+ * or without prior written permission, EQCOIN Foundation reserves all rights to
+ * take any legal action and pursue any right or remedy available under applicable
+ * law.
  * https://www.eqzip.com
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -22,8 +34,8 @@ import java.io.IOException;
 
 import org.bouncycastle.jcajce.provider.symmetric.ARC4.Base;
 
+import com.eqzip.eqcoin.keystore.AddressTool.AddressType;
 import com.eqzip.eqcoin.util.Base58;
-import com.eqzip.eqcoin.util.CRC8ITU;
 import com.eqzip.eqcoin.util.Log;
 
 /**
@@ -32,23 +44,24 @@ import com.eqzip.eqcoin.util.Log;
  * @email 10509759@qq.com
  */
 public class AddressTool {
-	public final static byte V1 = 0;
-	public final static int V1_PUBLICKEY_LEN = 33;
-	public final static byte V2 = 1;
-	public final static int V2_PUBLICKEY_LEN = 65;
-	public final static byte V3 = 2;
+	public final static int T1_PUBLICKEY_LEN = 33;
+	public final static int T2_PUBLICKEY_LEN = 67;
 	
+	public enum AddressType{
+		T1, T2, T3
+	}
 	
 	private AddressTool() {}
 	
 	/**
 	 * @param bytes compressed pubkey's EQCCHA hash. Each input is extended 100 times using MultipleExtend
+	 * @param type EQC Address’ type
 	 * @return EQC address
 	 */
-	public static String generateAddress(byte[] bytes, byte version) {
+	public static String generateAddress(byte[] bytes, AddressType type) {
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		// Calculate (version | trim(HASH))'s CRC8ITU
-		os.write(version);
+		// Calculate (type | trim(HASH))'s CRC8ITU
+		os.write(type.ordinal());
 		try {
 			os.write(trim(bytes));
 		} catch (IOException e) {
@@ -57,7 +70,7 @@ public class AddressTool {
 			Log.Error(e.getMessage());
 		}
 		byte crc = CRC8ITU.update(os.toByteArray());
-		// Generate address Base58(version) + Base58((trim(HASH) + (version | trim(HASH))'s CRC8ITU))
+		// Generate address Base58(type) + Base58((trim(HASH) + (type | trim(HASH))'s CRC8ITU))
 		os = new ByteArrayOutputStream();
 		try {
 			os.write(trim(bytes));
@@ -66,7 +79,7 @@ public class AddressTool {
 			e.printStackTrace();
 		}
 		os.write(crc);
-		return Base58.encode(new byte[] {version}) + Base58.encode(os.toByteArray());
+		return Base58.encode(new byte[] {(byte) type.ordinal()}) + Base58.encode(os.toByteArray());
 	}
 	
 	public static boolean verifyAddress(String address) {
@@ -113,6 +126,28 @@ public class AddressTool {
 		byte[] trim = new byte[j-i+1];
 		System.arraycopy(bytes, i, trim, 0, trim.length);
 		return trim;
+	}
+	
+	public static AddressType getAddressType(String address) {
+		byte type = 0;
+		AddressType addressType = AddressType.T1;
+		try {
+			type = Base58.decodeToBigInteger(address.substring(0, 1)).byteValue();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.Error(e.getMessage());
+		}
+		if(type == 0) {
+			addressType = AddressType.T1;
+		}
+		else if(type == 1) {
+			addressType = AddressType.T2;
+		}
+		else if(type == 2) {
+			addressType = AddressType.T3;
+		}
+		return addressType;
 	}
 	
 }
